dee71d3ba51bb0d312dfd84afff9b4fbd494ba60
GianHarleyCruz <giannis.gg43@gmail.com>
2026-09-15 18:17:52 +0800

    Added Gian's profile test

---
+++ app/src/androidTest/java/Quarter2/GianMinipeta.java
@@ -0,0 +1,16 @@
+package Quarter2;
+
+class GianMinipeta {
+    public void printmyProfile() {
+        String myName = "Gian";
+        String petName = "Max";
+        String favFood = "Porksteak";
+        int myage = 16;
+
+        System.out.println("--- My Digital Profile ---");
+        System.out.println("Hello! My name is " + myName + " and i am "  + myage);
+        System.out.println("i know a wonderful cat named " + petName + ".");
+        System.out.println("And one of my favorite food is " + favFood + ".");
+
+    }
+}
---
+++ app/src/test/java/com/example/studytask/studytask.java
@@ -0,0 +1,18 @@
+package com.example.studytask;
+
+import org.junit.Test;
+
+public class studytask {
+    @Test
+  public void printmyProfile() {
+        String myName = "Yani";
+        String petName = "Max";
+        String favFood = "Porksteak";
+        int myage = 16;
+
+        System.out.println("--- My Digital Profile ---");
+        System.out.println("Hello!, my name is " + myName + " and i am " + myage);
+        System.out.println("And I know a wonderful cat named " + petName + ".");
+        System.out.println("And one of my favorite food is " + favFood + ".");
+                 }
+    }
