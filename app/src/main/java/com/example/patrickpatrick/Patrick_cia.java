ec950fdad95bdb111331cdd44f6e890c59d89a69
limcuando <2015-60177@mcstayuman.edu.ph>
2026-09-17 17:53:33 +0800

    Patrick_67

---
+++ app/src/main/java/com/example/patrickpatrick/Patrick_cia.java
@@ -0,0 +1,21 @@
+package com.example.patrickpatrick;
+
+public class Patrick_cia {
+
+    public static void main(String[] args) {
+
+        String name = "Patricia Maire Decastro";
+        int age = 17;
+        String hobby = "Playing Roblox";
+        String favoriteFood = "Mcdo";
+        String goal = "To become better every day.";
+
+        System.out.println("===== ABOUT ME =====");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hobby: " + hobby);
+        System.out.println("Favorite Food: " + favoriteFood);
+        System.out.println("My Goal: " + goal);
+        System.out.println("====================");
+    }
+}
