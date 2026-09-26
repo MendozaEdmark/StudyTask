f6ed4697f343eabee4b65f06a912ac5541bde027
Zildjian Garcellano <zildjiangarcellano35@gmail.com>
2026-09-26 10:14:36 +0800

    Added GarcellanoMiniPeta.java profile test

---
+++ app/src/androidTest/java/Quarter2/GarcellanoMiniPeta.java
@@ -0,0 +1,18 @@
+package Quarter2;
+
+import org.junit.Test;
+
+public class GarcellanoMiniPeta {
+    @Test
+    public void printMyProfile() {
+        String myName = "Zildjian Garcellano";
+        String petName = "Olivia";
+        String favFood = "Adobo";
+        int myAge = 17;
+
+        System.out.println("--- My Digital Profile ---");
+        System.out.println("Hello!, my name is " + myName + " and i am " + myAge);
+        System.out.println("And I know a wonderful dog named " + petName + ".");
+        System.out.println("And one of my favorite food is " + favFood + ".");
+    }
+}
